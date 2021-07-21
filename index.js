1d33964c5ac3d01e56d62dbb98f7bb58e922bc36
filius-fall <sreeram596@gmail.com>
2021-07-21 14:05:52 +0530

    home page

---
+++ index.js
@@ -0,0 +1,37 @@
+// console.log('testing.......................')
+
+// scroll_function = function(){
+//     document.getElementById('head-nav').style.visibility = "hidden";
+// }
+
+// window.onscroll = function(){
+//     // scroll_function();
+//     document.getElementById('head-nav').style.opacity = 0;
+//     console.log("invisible")
+// }
+
+
+
+
+// var isScrolling;
+
+// // Listen for scroll events
+// window.addEventListener('scroll', function ( event ) {
+
+// 	// Clear our timeout throughout the scroll
+// 	window.clearTimeout( isScrolling );
+
+// 	// Set a timeout to run after scrolling ends
+// 	isScrolling = setTimeout(function() {
+
+// 		// Run the callback
+// 		console.log( 'Scrolling has stopped.' );
+//         // document.getElementById('head-nav').style.visibility = "visible";
+//         document.getElementById('head-nav').style.opacity = 1;
+
+
+
+// 	},300);
+
+// }, false);
+
